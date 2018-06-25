1e2999482cbe5dfcaa479cacedba4c2010197b72
orest.zamorylo <orest.zamorylo@WS-LV-CP1213>
2018-06-25 12:17:36 +0300

    First

---
+++ TestScreen/TestScreen/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestScreen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestScreen")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d82d307a-f271-4783-9d24-04ea123acf5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestScreen/TestScreen/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestScreen
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string fileName = "D:\\Screen\\abc.png";
+            var bound = Screen.GetBounds(Point.Empty);
+            using (var bitmap = new Bitmap(bound.Width, bound.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bound.Size);
+                    bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+
+            }
+        }
+    }
+}
